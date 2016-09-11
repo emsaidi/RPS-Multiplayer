60b87bd09bb516dae6fcb34c3b976e42d86a20bd
Saidi Granados <saidi.granados@gmail.com>
2016-09-11 11:18:34 -0500

    fixed it for real now

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -250,7 +250,7 @@ database.ref("Player1").child("chat").on("child_added", function (childSnapshot)
 	
 });
 
-database.ref('Player2').child("chat").on("child_changed", function (childSnapshot){
+database.ref('Player2').child("chat").on("child_added", function (childSnapshot){
  
 	var chatObject = childSnapshot.val().line;
 
